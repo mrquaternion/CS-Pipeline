8be23beac2b784bb03935338b242e682d0dd9fe5
mrquaternion <mathiaslarochelle1@gmail.com>
2025-05-23 11:57:52 -0400

    Code rearrangement

--- carbonpipeline/cli.py
+++ carbonpipeline/cli.py
@@ -13,145 +13,60 @@ from carbonpipeline.api_request import APIRequest
 import xarray as xr
 import pandas as pd
 from datetime import datetime
+from carbonpipeline.constants import *
 
 
-
-def processing(file: str, lat: float, lon: float):
+def processing(file: str, lat: float, lon: float, predictors: list[str] | None):
     """
     This function is intended to be used as the following.
 
     The attribute `file` in the argument of the CLI is CSV file. The latitude/longitude 
     attributs are meant to represent the coordinates of the EC tower the dataset is from.
-
-    The process is to find all the rows with missing values. Some datasets have all there 
-    rows with at least one missing value, others don't.
     """
-    column_mapping = {
-        'CO2_F_MDS': 'CO2',
-        'G_F_MDS':   'G',
-        'H_F_MDS':   'H',
-        'LE_F_MDS':  'LE',
-        'LW_IN_F':   'LW_IN',
-        'LW_OUT':    'LW_OUT',
-        'NETRAD':    'NETRAD',
-        'PA_F':      'PA',
-        'PPFD_IN':   'PPFD_IN',
-        'PPFD_OUT':  'PPFD_OUT',
-        'P_F':       'P',
-        'RH':        'RH',
-        'SW_IN_F':   'SW_IN',
-        'SW_OUT':    'SW_OUT',
-        'TA_F':      'TA',
-        'USTAR':     'USTAR',
-        'VPD_F':     'VPD',
-        'WD':        'WD',
-        'WS_F':      'WS',
-        'timestamp': 'timestamp'
-    }
 
     cs_dataset = pd.read_csv(file)
     df = cs_dataset.copy()
 
-    filtered_df = filtered_and_renamed_columns(df, column_mapping)
+    filtered_df = filtered_and_renamed_columns(df, COLUMN_NAME_MAPPING)
     filtered_df['timestamp'] = pd.to_datetime(filtered_df['timestamp'])
 
-    # keep only rows with any missing
-    miss = filtered_df[filtered_df[filtered_df.columns.drop('timestamp')].isnull().any(axis=1)].copy()
+    miss = missing_rows(filtered_df.copy()) # Keep only rows with any missing
 
-    # create new columns for year, month & day
-    miss['year']  = miss['timestamp'].dt.year
-    miss['month'] = miss['timestamp'].dt.month
-    miss['day'] = miss['timestamp'].dt.day
-
+    # -------------------------------------------------------------------------------------------------
+    nb_of_hours = 47                                        # ONLY THERE FOR TEST (1 day worth of data)
+    temp = miss.loc[:nb_of_hours]                           # ONLY THERE FOR TEST (1 day worth of data)
+    groups = list(temp.groupby(['year', 'month', 'day']))   # ONLY THERE FOR TEST (1 day worth of data)
+    # -------------------------------------------------------------------------------------------------
+    
     times = [f"{h:02d}:00" for h in range(24)]
+    preds = ERA5_VARIABLES if predictors is None else [var for pred in predictors for var in VARIABLES_FOR_PREDICTOR[pred]]
+    query_partial = partial(query, times=times, lat=lat, lon=lon, pred=preds) # Initialize the the arguments for the query
+    # groups = list(miss.groupby(['year', 'month', 'day']))
 
-    # multiprocessing for faster download
-    nb_of_hours = 47
-    temp = miss.loc[:nb_of_hours] # ONLY THERE FOR TEST (1 day worth of data)
-    groups = list(temp.groupby(['year', 'month', 'day']))
-    query_partial = partial(query, times=times, lat=lat, lon=lon)
-
+    # Multiprocessing for faster download
     start = time.time()
     with ProcessPoolExecutor(max_workers=4) as executor:
         executor.map(query_partial, groups)
     end = time.time()
-
     delta = end - start
     print(f"Time taken to download {(nb_of_hours + 1)/24} day worth of data: {delta} seconds")
         
-    df = merge_datasets()
-
-
-def rename_combined(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    To improve readability, the `shortName` columns in the original GRIB files are being renamed to 
-    their expanded names.
-    """
-    short_to_full = {
-        '10u': '10m_u_component_of_wind',
-        '10v': '10m_v_component_of_wind',
-        '2t': '2m_temperature',
-        '2d': '2m_dewpoint_temperature',
-        'sp': 'surface_pressure',
-        'tp': 'total_precipitation',
-        'avg_sdlwrf': 'mean_surface_downward_long_wave_radiation_flux',
-        'avg_sdlwrfcs': 'mean_surface_downward_long_wave_radiation_flux_clear_sky',
-        'avg_sdswrf': 'mean_surface_downward_short_wave_radiation_flux',
-        'avg_sdswrfcs': 'mean_surface_downward_short_wave_radiation_flux_clear_sky',
-        'ishf': 'instantaneous_surface_sensible_heat_flux',
-        'slhf': 'surface_latent_heat_flux',
-        'sshf': 'surface_sensible_heat_flux',
-        'stl1': 'soil_temperature_level_1',
-        'stl2': 'soil_temperature_level_2',
-        'stl3': 'soil_temperature_level_3',
-        'swvl1': 'volumetric_soil_water_layer_1',
-        'swvl2': 'volumetric_soil_water_layer_2',
-        'swvl3': 'volumetric_soil_water_layer_3',
-        'fal': 'forecast_albedo',
-        'zust': 'friction_velocity'
-    }
-    return df.rename(columns=short_to_full)
-
-
-def merge_datasets() -> pd.DataFrame:
-    """
-    Instead of querying for the whole time interval of the `predictors.csv` file, we queried for each day.
-
-    Now, merging every GRIB file together by `time`.
-    """
-    dataframes = []
-    directory = "./datasets/"
-    for name in os.listdir(directory): # source: https://www.geeksforgeeks.org/python-loop-through-folders-and-files-in-directory/
-        if name.endswith(".grib"):
-            path_to_file = os.path.join(directory, name)
-            ds_list = cfgrib.open_datasets(path_to_file, decode_timedelta=True)
-            ds = xr.merge(ds_list, compat="override")
-            df = ds.to_dataframe()
-            dataframes.append(df)
-
-    combined = pd.concat(dataframes, axis=1)
-    return rename_combined(combined)
-
+    df = merge_datasets()   
     
 
-def query(groupby_df: tuple, times: list[int], lat: float, lon: float):
-    """
-    Prepare the API request with args formatting and fetches the download.
-    """
-    (year, month, day), _ = groupby_df
-    request = APIRequest(
-            year=str(year),
-            month=f"{month:02d}",
-            day=f"{day:02d}",
-            time=times,
-            lat=lat,
-            lon=lon
-        )
-    
-    print(f"############ Download started for {year}-{month:02d}-{day:02d} ############")
-    request.fetch_download()
-    print(f"############ Download finished for {year}-{month:02d}-{day:02d} ############")
 
+
+
+def missing_rows(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Keeps rows with any cell with missing value and creates new columns (year, month & day) for easier request access.
+    """
+    miss = df[df[df.columns.drop('timestamp')].isnull().any(axis=1)]
+
+    miss['year']  = miss['timestamp'].dt.year
+    miss['month'] = miss['timestamp'].dt.month
+    miss['day'] = miss['timestamp'].dt.day
+    return miss
 
 def parse_timestamp(ts: str) -> Tuple[datetime.date, str]:
     dt = datetime.fromisoformat(ts)
@@ -166,17 +81,81 @@ def filtered_and_renamed_columns(df: pd.DataFrame, column_mapping: dict) -> pd.D
     return df[filtered_columns].rename(columns=column_mapping)
 
 
+def rename_combined_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Improve readability by renaming columns under the `shortName` naming given in the original GRIB file. Renamed to their expanded names.
+    
+    Parameters:
+        df (pd.DataFrame): The original DataFrame before processing.
+    
+    Returns:
+        pd.DataFrame. The DataFrame with renamed columns.
+    """
+    return df.rename(columns=SHORTNAME_TO_FULLNAME)
+
+
+def merge_datasets() -> pd.DataFrame:
+    """
+    Merges GRIB files after the download. Proceed with transforming them into DataFrames and renaming the column
+    for easier readibility.
+
+    Returns:
+        pd.DataFrame. The final DataFrame after merging the xr.DataSet objects.
+    """
+    dataframes = []
+    directory = "./datasets/"
+    for name in os.listdir(directory): # source: https://www.geeksforgeeks.org/python-loop-through-folders-and-files-in-directory/
+        if name.endswith(".grib"):
+            path_to_file = os.path.join(directory, name)
+            ds_list = cfgrib.open_datasets(path_to_file, decode_timedelta=True)
+            ds = xr.merge(ds_list, compat="override")
+            df = ds.to_dataframe()
+            dataframes.append(df)
+
+    combined = pd.concat(dataframes, axis=1)
+    return rename_combined_dataframe(combined)
+
+
+def query(groupby_df: tuple, times: list[str], lat: float, lon: float, predictors: list[str] | None):
+    """
+    Prepare the API request with args formatting and fetches the download.
+    """
+    (year, month, day), _ = groupby_df
+    request = APIRequest(
+            year=str(year),
+            month=f"{month:02d}",
+            day=f"{day:02d}",
+            time=times,
+            lat=lat,
+            lon=lon,
+            preds=predictors
+        )
+    
+    print(f"############ Download started for {year}-{month:02d}-{day:02d} ############")
+    request.fetch_download()
+    print(f"############ Download finished for {year}-{month:02d}-{day:02d} ############")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Pipeline to fill missing values in CarbonSense dataset.")
     parser.add_argument("--file", required=True, type=str, help="Path to the dataset file")
     parser.add_argument("--lat", required=True, type=int, help="Latitude coordinate")
     parser.add_argument("--lon", required=True, type=int, help="Longitude coordinate")
+    parser.add_argument("--preds", nargs='*', help="(Optional) List of predictors to use (e.g., --preds CO2 TA RH)")
     args = parser.parse_args()
+
+    if args.preds is not None:
+        invalid = []
+        VALID_PREDICTORS = list(VARIABLES_FOR_PREDICTOR.keys())
+        for pred in args.preds:
+            if pred not in VALID_PREDICTORS:
+                invalid.append(pred)
+        parser.error(f"\nInvalid predictor(s): {', '.join(invalid)} \nValid options are: {', '.join(VALID_PREDICTORS)}")
 
     print(f"File: {args.file}")
     print(f"Lat: {args.lat}, Lon: {args.lon}")
 
-    processing(args.file, args.lat, args.lon)
+    processing(args.file, args.lat, args.lon, args.preds)
 
 if __name__ == "__main__":
     main()
